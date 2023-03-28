18a759c21b792570a6d3aa523a393980b008d6ba
ariful4082 <ariful4082@gmail.com>
2023-03-28 15:26:41 +0600

    update repo code

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
+import { StoreProvider } from "easy-peasy";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: "",
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: "",
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StoreProvider value="store">
